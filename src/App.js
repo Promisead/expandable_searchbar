c42d84fb90654c0d097330b1a42f970236dd032d
Promise Champion <promiseduke@gmail.com>
2022-10-25 16:46:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Search from './components/search';
+import './styles.css';
 
-function App() {
+const Title = styled.p`
+  font-size: 2rem;
+  color: #eeeeee;
+  letter-spacing: 0.15em;
+  line-height: 2em;
+`;
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>Expanding Search Box</Title>
+      <Search />
     </div>
   );
 }
-
-export default App;
